40620a5f7fa307fdf9cca2861948ce3544555996
juanmachi <q.juanmanu@gmail.com>
2023-04-03 21:22:42 -0300

    nuevo

---
+++ app.js
@@ -0,0 +1,8 @@
+function imprimir() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var comentario = document.getElementById("comentario").value;
+  
+    console.log("Nombre: " + nombre);
+    console.log("email: " + email);
+    console.log("comentario: " + comentario);}
