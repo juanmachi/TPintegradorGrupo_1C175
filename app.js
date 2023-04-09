aa56698ff3a545c1364b6ef342e3c7f445179d0c
Vanina M Herrera <vanyherr@gmail.com>
2023-04-08 23:46:58 -0300

    cambio en formulario-focus y limpiacampos

--- app.js
+++ app.js
@@ -5,4 +5,26 @@ function imprimir() {
   
     console.log("Nombre: " + nombre);
     console.log("email: " + email);
-    console.log("comentario: " + comentario);}
+    console.log("comentario: " + comentario);
+    resetform();
+    enfocar();
+}
+   
+    function resetform() {
+        //Limpia los campos 
+        $("form input[type=email]").each(function() { this.value = '' });
+        $("form input[type=text] , form textarea ").each(function() { this.value = '' });
+   }
+   function enfocar()
+   { 
+    //Coloca el cursor en los input
+    const $btnEnfocar = document.querySelector("#btnEnfocar"),
+	$nombre = document.querySelector("#nombre");
+
+// En el click, enfocar
+$btnEnfocar.addEventListener("click", () => {
+	$nombre.focus();
+});
+
+   
+}
