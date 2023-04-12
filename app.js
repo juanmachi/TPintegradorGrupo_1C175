d684713b7fabb1313600aaa49625d49d1f724583
Vanina M Herrera <vanyherr@gmail.com>
2023-04-11 23:36:23 -0300

    api-

--- app.js
+++ app.js
@@ -25,6 +25,49 @@ function imprimir() {
 $btnEnfocar.addEventListener("click", () => {
 	$nombre.focus();
 });
-
    
+}
+//api
+function clima() {
+    let ciudad = $("input")[0].value;
+  
+    $.getJSON(
+      "https://api.openweathermap.org/data/2.5/weather?lang=es&q=" +
+        ciudad +
+        "&appid=dcec7df661b1e6b0edab51d796b7339c",
+      function (data) {
+        console.log(data);
+  
+        document.getElementById("ciudad").textContent = data.name;
+        document.getElementById("grados").textContent =
+          "Grados: " + parseInt(data.main.temp - 273.15);
+        document.getElementById("imgIco").setAttribute( "src",  `https://openweathermap.org/img/wn/${data.weather[0].icon}.png` );
+        document.getElementById("clima").textContent = data.weather[0].description ;
+      }
+    );
+   
+  }
+  
+  document.querySelector("button").addEventListener("click", function () {
+    clima();
+    resetinp();
+    enfocar1();
+  });
+
+  function resetinp() {
+    //Limpia los campos 
+   
+    $("label input[type=text]  ").each(function() { this.value = '' });
+}
+function enfocar1()
+{ 
+//Coloca el cursor en los input
+const $btnEnfocar2 = document.querySelector("#bot2"),
+$nom6 = document.querySelector("#city");
+
+// En el click, enfocar
+$btnEnfocar2.addEventListener("click", () => {
+$nom6.focus();
+});
+
 }
